3da2ccaa92b1a64c423c5c93aba4a1bc025609e7
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-01-17 17:31:40 +0530

    Added spaces and ouput headings to add the output in the form of comments

--- JS_LEC-2_HW_QUES/script.js
+++ JS_LEC-2_HW_QUES/script.js
@@ -1,18 +1,53 @@
-console.log(window);
+console.log(window);   
+// OUTPUT : 
+
 console.log(1+"1");
+// OUTPUT : 
+
 console.log(1-"1");
+// OUTPUT : 
+
 console.log("1"-1);
+// OUTPUT : 
+
 console.log("5"*"2");
+// OUTPUT : 
+
 console.log(true+true);
+// OUTPUT : 
+
 console.log(true-false);
+// OUTPUT : 
+
 console.log(!!false);
+// OUTPUT : 
+
 console.log(!!"false");
+// OUTPUT : 
+
 console.log(!![]);
+// OUTPUT : 
+
 console.log(![]);
+// OUTPUT : 
+
 console.log([]+[]);
+// OUTPUT : 
+
 console.log([1,2]+[3,4]);
+// OUTPUT : 
+
 console.log([]+{});
+// OUTPUT : 
+
 console.log(0.1+0.2==0.3);
+// OUTPUT : 
+
 console.log(0.1+0.2===0.3);
+// OUTPUT : 
+
 console.log([2]==[2]);
+// OUTPUT : 
+
 // console.log([2]===[2]);
+// OUTPUT : 
