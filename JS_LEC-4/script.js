8c07e779f315bf20e2e33bb50dd685ad60235bb3
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-01-19 15:37:37 +0530

    added question-1 in script.js

---
+++ JS_LEC-4/script.js
@@ -0,0 +1,11 @@
+// QUESTION - 1 :
+// Reverse the array by using push and pop method
+let arr=[1,2,3,4,5]
+let rev=[]
+while (arr.length>0){
+    rev.push(arr.pop())
+}
+console.log(reverse)
+
+// QUESTION - 2 :
+// Remove all the negative numbers from an array
