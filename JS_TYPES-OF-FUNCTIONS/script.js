ee31b2e7764d01674040d880a6d23157d79b8341
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-01-24 19:19:12 +0530

    Added the output heading in the form of comments

--- JS_TYPES-OF-FUNCTIONS/script.js
+++ JS_TYPES-OF-FUNCTIONS/script.js
@@ -5,15 +5,18 @@ function greet(name){
     return `Hello, ${name}`;
 }
 console.log(greet("Manpreet"))
+// Output :
 
 // 2. ARROW FUNCTION 
 const greet2=(name)=>`Hello, ${name}!`;
 console.log(greet2("Manpreet"))
+// Output :
 
 // 3. ANONYMOUS FUNCTION : functions without name.
 setTimeout(function() {
     console.log("Hello, world!");
 }, 1000);
+// Output :
 
 // 4. FUNCTION EXPRESSION : this involve defining a function and assigning it to a 
 // variable. They can be named or anonymous.
@@ -21,6 +24,7 @@ const greet3 = function(name) {
     return `Hello, ${name}!`;
 };
 console.log(greet3("Manpreet"));
+// Output :
 
 // 5. CALLBACK FUNCTION : this function is passed as an argument to another function
 // and is executed after some opertaion has been compleleted.
@@ -31,6 +35,7 @@ function executeCallback(callback) {
     callback();
 }
 executeCallback(logMessage);
+// Output :
 
 // 6. FIRST-CLASS FUNCTION :  JavaScript treats functions as first-class citizens,
 //  meaning they can be assigned to variables, passed as arguments, and returned from
@@ -40,6 +45,7 @@ function greet4(name) {
 }
 const sayHello = greet4;
 console.log(sayHello("Manpreet"));
+// Output :
 
 // 7. HIGHER-ORDER FUNCTION : A higher-order function is a function that accepts
 //  other functions as arguments or returns a function.
@@ -50,6 +56,7 @@ function logMessage() {
     console.log("This is a higher-order function.");
 }
 higherOrderFunction(logMessage);
+// Output :
 
 // 8. SELF-INVOKING FUNCTION (IIFE) : A self-invoking function, also known as an 
 // Immediately Invoked Function Expression (IIFE), is a function that is executed 
@@ -57,4 +64,5 @@ higherOrderFunction(logMessage);
 (function() {
     console.log("This is an IIFE.");
 })();
+// Output :
 
