9293f77588141cba144b9dbcdbd96066d5e8d847
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-01-25 21:40:20 +0530

    Added some spaces in the file

--- JS_TYPES-OF-FUNCTIONS/script.js
+++ JS_TYPES-OF-FUNCTIONS/script.js
@@ -1,5 +1,6 @@
 // DIFFERENT TYPES OF FUNCTION IN JS :-
 
+
 // 1. REGULAR FUNCTION : Defined using "function" keyword.
 function greet(name){
     return `Hello, ${name}`;
@@ -7,16 +8,22 @@ function greet(name){
 console.log(greet("Manpreet"))
 // Output : Hello, Manpreet
 
+
+
 // 2. ARROW FUNCTION 
 const greet2=(name)=>`Hello, ${name}!`;
 console.log(greet2("Manpreet"))
 // Output : Hello, Manpreet!
+
+
 
 // 3. ANONYMOUS FUNCTION : functions without name.
 setTimeout(function() {
     console.log("Hello, world!");
 }, 1000);
 // Output : Hello, world!
+
+
 
 // 4. FUNCTION EXPRESSION : this involve defining a function and assigning it to a 
 // variable. They can be named or anonymous.
@@ -25,6 +32,8 @@ const greet3 = function(name) {
 };
 console.log(greet3("Manpreet"));
 // Output : Hello, Manpreet!
+
+
 
 // 5. CALLBACK FUNCTION : this function is passed as an argument to another function
 // and is executed after some opertaion has been compleleted.
@@ -37,6 +46,8 @@ function executeCallback(callback) {
 executeCallback(logMessage);
 // Output : This is a callback function.
 
+
+
 // 6. FIRST-CLASS FUNCTION :  JavaScript treats functions as first-class citizens,
 //  meaning they can be assigned to variables, passed as arguments, and returned from
 //  other functions.
@@ -46,6 +57,8 @@ function greet4(name) {
 const sayHello = greet4;
 console.log(sayHello("Manpreet"));
 // Output : Hello, Manpreet!
+
+
 
 // 7. HIGHER-ORDER FUNCTION : A higher-order function is a function that accepts
 //  other functions as arguments or returns a function.
@@ -57,6 +70,8 @@ function logMessage() {
 }
 higherOrderFunction(logMessage);
 // Output : This is a higher-order function.
+
+
 
 // 8. SELF-INVOKING FUNCTION (IIFE) : A self-invoking function, also known as an 
 // Immediately Invoked Function Expression (IIFE), is a function that is executed 
