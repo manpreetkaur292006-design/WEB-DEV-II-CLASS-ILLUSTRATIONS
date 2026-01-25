98a407d5a56e04ba4f613a00fdf679b53aa13c75
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-01-25 19:56:45 +0530

    modified some comments and added some spaces to increase the code readability

--- JS_LEC-5/script.js
+++ JS_LEC-5/script.js
@@ -1,3 +1,6 @@
+// SPREAD OPERATOR
+
+
 // spread opertaor is the other way to concatenate the two arrays
 // spread operator expands the array and strings and gives the access of individual element
 
@@ -6,14 +9,19 @@ fruit2=["Orange","papaya"]
 console.log(fruit1.concat(fruit2))  // using concat 
 console.log([...fruit1,...fruit2]) // using the spread operator
 
+
+
 // QUESTION - 3 :
+
 // check that array is pallindrome or not
+
 function pallindrome(){
     let arr=[1,2,3,2,1]
     let original=[...arr] // cloning the arr to the original matlab poora array original me transfer ho jayega
     let reverse=[]
 
-    // // push the given array elements into original array
+    // ALTERNATE WAY :
+    // // push the given array elements into original array 
     // for (let i=0; i<arr.length; i++){
     //     original.push(arr[i])
     // }
@@ -34,33 +42,47 @@ function pallindrome(){
 console.log(pallindrome())
 // Output : Pallindrome
 
+
 // ternary operator (conditional operator)
+
 // syntax :- condition ? true : false
+
 let age = 18;
 let canvote=age>=18?"you can vote":"you can't vote"
 console.log(canvote)
 
+
+
 // question : find that the number is even or odd
+
 let num=234;
 let evo=num%2===0?"Even":"odd"
 console.log(evo)
 
+
+
 // question : check a year is leap year or not
+
 let year=2026;
 let leap=(year%4===0 && year%100!==0) || (year%400===0) ?"leap":"not leap"
 console.log(leap)
 
+
+
 // array method - reverse()
 // and includes() - used in api and credential checking 
 
+
 // string methods
 // homework - practise the string methods and push in the repo
+
 
 // types of functions {continue}
 
 // first class function
 
-// first point sample
+
+// first point sample :- ALL THE ARE POINTS WRITTEN ON THE NOTE BOOK
 const demo=function(){
 
 }
@@ -69,8 +91,10 @@ const demo1=()=>{
 
 }
 
+
 // second point sample 
-// see callback function example
+// see callback function example - EXAMPLE SAME AS THE CALLBACK FUNCTION
+
 
 // third point sample
 function sample(){
@@ -84,6 +108,7 @@ function sample(){
 
 var value=sample();
 value() // aisa same arrow function me bhi hota hai jahan value variable ko call kara ha raha hai
+
 
 function sample1(){
     console.log("sample")  // we can console here
@@ -102,6 +127,7 @@ let value2=value()
 value2();
 
 
+
 // anonymous function
 // example - settimeout and setinterval and date method
  
@@ -113,6 +139,7 @@ setTimeout(()=>{
 setInterval(()=>{
     console.log("Manpreet");
 },5000)
+
 
 
 // self invoking functions
