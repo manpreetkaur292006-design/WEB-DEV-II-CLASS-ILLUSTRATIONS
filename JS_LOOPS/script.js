c05da5c645dfc850cce14cbe1891b2caf36df7c5
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-01-26 12:15:56 +0530

    Added the first type of for loop in the file

--- JS_LOOPS/script.js
+++ JS_LOOPS/script.js
@@ -1,8 +1,19 @@
 // LOOPS IN JS
 
 // IN JS LOOPS ARE OF TWO TYPES :
-// 1. FOR LOOP
-//    A. FOR LOOP
+// 1. FOR LOOP : The for loop repeats a block of code a specific number of times. It contains
+//  initialization, condition, and increment/decrement in one line.
+
+//    A. FOR LOOP :
+
+// SYNTAX :-
+// for (initialization; condition; increment/decrement)  {// Code to execute}
+
+// EXAMPLE : 
+for (let i = 1; i <= 3; i++) {
+    console.log("Count:", i);
+}
+
 //    B. FOR IN LOOP
 //    C. FOR EACH LOOP
 
