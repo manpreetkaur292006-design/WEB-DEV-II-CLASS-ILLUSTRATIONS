9cd429bceb7b25a5f510ced1b42bbbc4b3549000
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-01-27 11:44:40 +0530

    Added the examples of all the types of the statements in script.js

--- JS_IF-ELSE/script.js
+++ JS_IF-ELSE/script.js
@@ -6,7 +6,11 @@
                 // Code to execute if the condition is true
 //          }
 
-// EXAMPLE :
+// EXAMPLE : 
+let age = 20;
+if (age >= 18) {
+    console.log("You are an adult.");
+}
 
 // 2. IF-ELSE STATEMENT
 
@@ -17,6 +21,12 @@
 //          }
 
 // EXAMPLE :
+let isRaining = true;
+if (isRaining) {
+    console.log("Bring an umbrella.");
+} else {
+    console.log("No need for an umbrella.");
+}
 
 // 3. NESTED IF-ELSE STATEMENT
 
@@ -32,6 +42,17 @@
             // }
 
 // EXAMPLE :
+let score = 75;
+if (score >= 60) {
+    console.log("You passed.");
+    if (score >= 90) {
+        console.log("You got an A grade!");
+    } else {
+        console.log("You got a B grade.");
+    }
+} else {
+    console.log("You failed.");
+}
 
 // 4. CASCADED IF-ELSE STATEMENT
 
@@ -46,6 +67,16 @@
             // }
 
 // EXAMPLE :
+let grade = 'B';
+if (grade === 'A') {
+    console.log("Excellent!");
+} else if (grade === 'B') {
+    console.log("Good job!");
+} else if (grade === 'C') {
+    console.log("You can do better.");
+} else {
+    console.log("Needs improvement.");
+}
 
 // 5. SWITCH CASE STATEMENT
 
@@ -60,6 +91,17 @@
                 // default:
                     // Code to execute if none of the cases match
                 // }
-                
+
 // EXAMPLE :
+let dayOfWeek = 'Monday';
+switch (dayOfWeek) {
+    case 'Monday':
+        console.log("It's the start of the week.");
+        break;
+    case 'Friday':
+        console.log("Weekend is almost here!");
+        break;
+    default:
+        console.log("It's a weekday.");
+}
 
