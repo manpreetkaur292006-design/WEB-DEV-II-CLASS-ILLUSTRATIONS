e740bcdf5c8c9c81efb6b5aedfb1bc8bbe4c7e56
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-01-27 11:39:28 +0530

    Added the syntax of all the statements in the form of comments

--- JS_IF-ELSE/script.js
+++ JS_IF-ELSE/script.js
@@ -1,7 +1,65 @@
 // DECISION MAKING STATEMENTS IN JS
 
 // 1. IF STATEMENT
+
+// SYNTAX : if (condition) {
+                // Code to execute if the condition is true
+//          }
+
+// EXAMPLE :
+
 // 2. IF-ELSE STATEMENT
+
+// SYNTAX : if (condition) {
+                // Code to execute if the condition is true
+//          } else {
+//              // Code to execute if the condition is false
+//          }
+
+// EXAMPLE :
+
 // 3. NESTED IF-ELSE STATEMENT
+
+// SYNTAX : if (condition1) {
+                // Code to execute if condition1 is true
+            // if (condition2) {
+                // Code to execute if condition2 is true
+            // } else {
+                // Code to execute if condition2 is false
+            // }
+            // } else {
+                // Code to execute if condition1 is false
+            // }
+
+// EXAMPLE :
+
 // 4. CASCADED IF-ELSE STATEMENT
+
+// SYNTAX : if (condition1) {
+                // Code to execute if condition1 is true
+            // } else if (condition2) {
+                // Code to execute if condition2 is true
+            // } else if (condition3) {
+                // Code to execute if condition3 is true
+            // } else {
+                // Code to execute if none of the conditions are true
+            // }
+
+// EXAMPLE :
+
 // 5. SWITCH CASE STATEMENT
+
+// SYNTAX : switch (expression) {
+                // case value1:
+                    // Code to execute if expression === value1
+                    // break;
+                // case value2:
+                    // Code to execute if expression === value2
+                    // break;
+                // More cases...
+                // default:
+                    // Code to execute if none of the cases match
+                // }
+                
+// EXAMPLE :
+
