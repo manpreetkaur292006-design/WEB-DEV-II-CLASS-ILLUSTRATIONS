8893682be97055d1e3786926a470508f3e0b9c28
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-01-29 22:25:01 +0530

    Added the definations of the break and continue statements in the form of comments

---
+++ JS_LEC-8/JS_JUMP_STATEMENTS/script.js
@@ -0,0 +1,11 @@
+// JUMP STATEMENTS OR CONTROL FLOW STATEMENTS IN JS
+
+// 1. BREAK :
+//  The break statement is used to exit a loop prematurely
+//  when a certain condition is met. It is commonly used to 
+//  terminate a loop early, preventing further iterations.
+
+// 2. CONTINUE :
+//  The continue statement is used to skip the current iteration of
+//  a loop and proceed to the next iteration. It allows you to skip
+//  over specific values or conditions without terminating the loop entirely.
