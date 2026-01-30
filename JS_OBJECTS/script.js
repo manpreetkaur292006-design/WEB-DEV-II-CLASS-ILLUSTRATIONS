75bbf3e529f955722960007196d7f7fa96dc13ed
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-01-30 21:51:30 +0530

    Added the code explanation in the form of comments

--- JS_OBJECTS/script.js
+++ JS_OBJECTS/script.js
@@ -6,23 +6,27 @@
 
 // EXAMPLE :
 
-let student = {
+const student = {
     Sname : "Manpreet Kaur",
     Sroll : 2501010070,
     age : 20,
     dept : "SOET"
 }
 
-console.log(student)
+console.log(student)  // displaying the whole object 
+ 
+console.log(student.Sname)  // displaying the value with key Sname in first way
+console.log(student["age"])  // displaying the value with the key age in second way
 
-console.log(student.Sname)
-console.log(student["age"])
+console.log(student.Sroll)  // displaying the value with the key Sroll
+console.log(student["dept"])  // displaying the value with the key dept
 
-console.log(student.Sroll)
-console.log(student["dept"])
+student.Sname = "Manpreet"  // modifying the value with key Sname
+console.log(student.Sname)  // displaying the changes entity
 
-student.Sname = "Manpreet"
-console.log(student.Sname)
+console.log(student)   // displaying the object again
 
-console.log(student)
+// here while creating the object i have used the "const" but still we are able
+// to modify the inner entity as we are not modifying the whole object but only one or
+// more inner entities not the whole object
 
