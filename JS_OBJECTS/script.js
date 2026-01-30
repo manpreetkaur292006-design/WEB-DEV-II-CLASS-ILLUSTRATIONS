cb8e7e38953647acdc09bf8c972c322bbc8647f3
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-01-30 21:44:42 +0530

    Added the definition and the example of objects in js

---
+++ JS_OBJECTS/script.js
@@ -0,0 +1,28 @@
+
+// JS OBJECTS
+
+// They are collections of key-value pairs where each key is a string
+// (or a symbol) and each value can be any data type, including other objects.
+
+// EXAMPLE :
+
+let student = {
+    Sname : "Manpreet Kaur",
+    Sroll : 2501010070,
+    age : 20,
+    dept : "SOET"
+}
+
+console.log(student)
+
+console.log(student.Sname)
+console.log(student["age"])
+
+console.log(student.Sroll)
+console.log(student["dept"])
+
+student.Sname = "Manpreet"
+console.log(student.Sname)
+
+console.log(student)
+
