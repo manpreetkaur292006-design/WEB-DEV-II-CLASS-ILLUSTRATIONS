310ea8de2e81ec29b26f1d92fe05dc8ef06b289d
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-01-31 10:29:20 +0530

    Added the solution of the third and fourth question

--- JS_PRACTISE-1/script.js
+++ JS_PRACTISE-1/script.js
@@ -17,8 +17,21 @@ for (let j=0; j<arr1.length; j++){
 }
 
 // 3. ADD 3 TO EACH ELEMENT IN AN ARRAY
+let arr2=[2,3,41,53,56,11,12,9]
+for (let k=0; k<arr2.length; k++){
+    arr2[k]=arr2[k]+3
+    console.log(arr2[k])
+}
 
 // 4. FIND OUT THE MAXIMUM ELEMENT IN AN ARRAY
+let arr3=[1,23,43,12,190,12,9]
+let max=arr3[0]
+for (let p=0; p<arr3.length; p++){
+    if (arr3[p]>max){
+        max=arr3[p]
+    }
+}
+console.log(max)
 
 // 5. SORT THE NUMBERS IN AN ARRAY
 
