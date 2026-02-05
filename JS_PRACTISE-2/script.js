47c6cbc9f225f2b5fa2a9373de3aa83b073a1fcd
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-05 19:59:15 +0530

    Added two questions in the file

---
+++ JS_PRACTISE-2/script.js
@@ -0,0 +1,19 @@
+
+// QUESTIONS BASED ON JS STRINGS
+
+// 1. FIND ALL THE CHARACTERS IN THE STRING
+let s="Manu";
+for (let i=0; i<s.length; i++){
+    console.log(s[i])
+}
+
+// 2. REVERSE A STRING
+let s2="Manpreet Kaur"
+rev=""
+for (let j=s2.length-1; j>=0; j--){
+    rev+=s2[j];
+}
+console.log(rev)
+
+// 3. RETRIVE A STRING FROM A GIVEN STRING
+
