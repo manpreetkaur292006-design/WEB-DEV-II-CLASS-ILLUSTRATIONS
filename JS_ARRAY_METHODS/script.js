ff498486d6581d5ca252f61579a56f9403be0e52
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-08 16:40:49 +0530

    Added the names of all the array methods in the form of comments

---
+++ JS_ARRAY_METHODS/script.js
@@ -0,0 +1,18 @@
+
+// ARRAY METHODS IN JS 
+
+// 1. map(callback) :
+// 2. filter(callback) :
+// 3. reduce(callback) :
+// 4. indexOf(element) :
+// 5. push(element) :
+// 6. pop() :
+// 7. shift() :
+// 8. unshift(element) :
+// 9. slice(start,end) :
+// 10. some(callback) :
+// 11. every(callback) :
+// 12. concat(array) :
+// 13. splice(start,deleteCount,item1,item2, ...) :
+// 14. reverse() :
+// 15. includes() :
