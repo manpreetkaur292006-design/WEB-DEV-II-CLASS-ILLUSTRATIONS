2468f042858e41240218ba853572c0810b3c8174
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-10 15:30:53 +0530

    made some changes in the files

---
+++ ASSIGNMENT-1/script.js
@@ -0,0 +1,25 @@
+
+// TARGET THE FORM AND BUTTONS
+
+const eventTitle=document.getElementById("Event-Title")
+const eveDate=document.getElementById("Event-Date")
+const cat=document.getElementById("Category")
+const desc=document.getElementById("Description")
+const b1=document.getElementById("b1")
+const b2=document.getElementById("b2")
+const b3=document.getElementById("b3")
+
+const events=[
+    {
+        title:"x",
+        date:29-jan-2026,
+        category:"Social",
+        description:"hui"
+    },
+    {
+        title:"x",
+        date:29-jan-2026,
+        category:"Social",
+        description:"hui"
+    }
+]
