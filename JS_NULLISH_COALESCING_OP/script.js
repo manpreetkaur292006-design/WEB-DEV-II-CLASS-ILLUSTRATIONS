d6d9283e13d9274962b121e42c15338e1cea9b8d
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-12 21:57:07 +0530

    Added another example in the code

--- JS_NULLISH_COALESCING_OP/script.js
+++ JS_NULLISH_COALESCING_OP/script.js
@@ -5,9 +5,15 @@
 // operand when the left-hand operand is null or undefined; 
 // otherwise, it returns the left-hand operand.
 
-
+// EXAMPLE-1 
 let value = null;
 let defaultValue = "Default Value";
 let result = value ?? defaultValue;
 console.log(result);
+
+// EXAMPLE-2
+let val="Value";
+let defVal="Default";
+let output=val ?? defVal;
+console.log(output)
 
