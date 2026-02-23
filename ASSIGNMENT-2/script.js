5bd0a568815d67fbccdab9df2df42307465a47be
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-23 15:49:23 +0530

    added the code in the file

--- ASSIGNMENT-2/script.js
+++ ASSIGNMENT-2/script.js
@@ -1,14 +1,14 @@
 
 // weather api 
 
-// const options = {method: 'GET', headers: {Authorization: 'Bearer zpka_208202a3448e45da80072ace8ef7d7d0_a3c48253'}};
+// const options = {method: 'GET', headers: {Authorization: 'Bearer key'}};
 
 // fetch('https://dataservice.accuweather.com/locations/v1/cities/search?q=%3Cstring%3E', options)
 //   .then(response => response.json())
 //   .then(response => console.log(response))
 //   .catch(err => console.error(err));
 
-const options = {method: 'GET', headers: {Authorization: 'Bearer zpka_208202a3448e45da80072ace8ef7d7d0_a3c48253'}};
+const options = {method: 'GET', headers: {Authorization: 'Bearer key'}};
 
 fetch('https://dataservice.accuweather.com/currentconditions/v1/:202396', options)
   .then(response => response.json())
