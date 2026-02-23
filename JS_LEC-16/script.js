1271acdadd073f6dbac5976c91950bb8dac02dc4
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-23 15:47:15 +0530

    Added today's code in the repo

--- JS_LEC-16/script.js
+++ JS_LEC-16/script.js
@@ -1,1 +1,31 @@
-g
+
+// EVENT LOOPS
+// how js works behind the scene
+
+console.log("first line")
+
+
+setTimeout(()=>{
+    console.log("inside timeout")
+},0)
+
+
+const p=new Promise((resolve,reject)=>{
+    resolve()
+})
+p.then(()=>{
+    console.log("inside promise")
+})
+
+
+const p2=new Promise((resolve,reject)=>{
+    resolve()
+})
+p2.then(()=>{
+    console.log("inside promise")
+})
+
+
+console.log("last line")
+
+
