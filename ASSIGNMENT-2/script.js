ba11d6b897c6681992cf1979e65883bf64a3b89b
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-24 14:35:18 +0530

    added the code in the js file

--- ASSIGNMENT-2/script.js
+++ ASSIGNMENT-2/script.js
@@ -1,16 +1,21 @@
 
 // weather api 
 
-// const options = {method: 'GET', headers: {Authorization: 'Bearer key'}};
-
-// fetch('https://dataservice.accuweather.com/locations/v1/cities/search?q=%3Cstring%3E', options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
-
+function getlocation(city){
 const options = {method: 'GET', headers: {Authorization: 'Bearer key'}};
 
-fetch('https://dataservice.accuweather.com/currentconditions/v1/:202396', options)
+fetch('https://dataservice.accuweather.com/locations/v1/cities/search?q=%3Cstring%3E', options)
   .then(response => response.json())
   .then(response => console.log(response))
   .catch(err => console.error(err));
+
+fetch('https://dataservice.accuweather.com/currentconditions/v1/%city', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+}
+
+
+
+  // wrap bot the things in the functions and we can call them accordingly
+  
