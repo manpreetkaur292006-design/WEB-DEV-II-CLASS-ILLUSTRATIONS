e37b83d2697197e1142984f348909859cfa9aa53
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-28 22:16:11 +0530

    Added a comment

--- ASSIGNMENT-2/script.js
+++ ASSIGNMENT-2/script.js
@@ -1,5 +1,6 @@
 
-// weather api 
+// weather api
+// async wether tracker
 
 function getlocation(city){
 const options = {method: 'GET', headers: {Authorization: 'Bearer zpka_208202a3448e45da80072ace8ef7d7d0_a3c48253'}};
@@ -17,5 +18,5 @@ fetch('https://dataservice.accuweather.com/currentconditions/v1/%city', options)
 
 
 
-  // wrap botH the things in the functions and we can call them accordingly
+  // wrap both the things in the functions and we can call them accordingly
   
