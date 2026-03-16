6f71b028d9fd3eea9373d756cdf6f877c3f98c00
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-03-16 19:30:54 +0530

    fetched data using api of openweather

--- ASSIGNMENT-2/script.js
+++ ASSIGNMENT-2/script.js
@@ -5,4 +5,20 @@
 
 // WE ARE HERE USING OPEN WEATHER API
 
+// API key :- 2021306ceb8064a5730e274654b326c6
 
+// BUILD AN API REQUEST USING CITY NAME :
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+async function getData(city) {
+  try{
+    const API_Key="2021306ceb8064a5730e274654b326c6";
+    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}
+`);
+      const data=await response.json();
+      console.log(data);
+  }catch(err){
+    console.log(err);
+  }
+}
+getData("Haryana")
