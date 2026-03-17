aed66c77963765182179a04531d2d492929cd44e
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-03-17 15:05:13 +0530

    fixed some errors in the code

--- ASSIGNMENT-2/script.js
+++ ASSIGNMENT-2/script.js
@@ -11,15 +11,19 @@
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric 
 // &units=metric  : to get temperature in degree celsius
 
-async function getData(city) {
-  try{
-    const API_Key="2021306ceb8064a5730e274654b326c6";
-    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}
-`);
-      const data=await response.json();
-      console.log(data);
-  }catch(err){
-    console.log(err);
-  }
+
+async function fetchData(city) {
+    try {
+        const API_key = "2021306ceb8064a5730e274654b326c6";
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`);
+
+        const data = await response.json();
+        console.log(city);
+        console.log(data.main.temp);
+        console.log(data.main.humidity);
+    } catch (err) {
+        console.error(err);
+    }
 }
-getData("Haryana")
+fetchData("london")
+
